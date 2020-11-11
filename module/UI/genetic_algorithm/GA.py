5e8d758967a9580991d58f344c383a2b1504c7b1
Ga Man Liang <g.liang@campus.tu-berlin.de>
2020-11-11 14:26:45 +0100

    ref #28 : adding custom mutation so genes stay in defined range

--- module/UI/genetic_algorithm/GA.py
+++ module/UI/genetic_algorithm/GA.py
@@ -19,146 +19,151 @@
 
 import random
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 from deap import base
 from deap import creator
 from deap import tools
+from deap import benchmarks
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+# min / max, fixed(bool)
+genes = np.array([
+    [0.85, 0.95, 0, "pp"],  # PP
+    [-0.1, 0.1, 0, "ao"],  # AO
+    [0.43, 0.43, 1, "div"],  # division
+    [18., 18., 1, "alph1"],  # alpha1
+    [23., 23., 1, "alph2"],  # alpha2
+    [23., 23., 1, "lambd"],  # lambd
+    [0.0477, 0.0477, 1, "th"],  # thickness
+    [0.4, 0.4, 1, "xmaxth"],  # xmaxth
+    [0.3742, 0.3742, 1, "xmaxch"],  # xmaxcamber
+    [0.01, 0.01, 1, "leth"],  # LE thickness
+    [0.01, 0.01, 1, "teth"]  # TE thickness
+])
 
-toolbox = base.Toolbox()
-
-# Attribute generator
-#                      define 'attr_bool' to be an attribute ('gene')
-#                      which corresponds to integers sampled uniformly
-#                      from the range [0,1] (i.e. 0 or 1 with equal
-#                      probability)
-toolbox.register("attr_bool", random.randint, 0, 1)
-
-# Structure initializers
-#                         define 'individual' to be an individual
-#                         consisting of 100 'attr_bool' elements ('genes')
-toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.attr_bool, 100)
-
-# define the population to be a list of individuals
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-
-# the goal ('fitness') function to be maximized
-def evalOneMax(individual):
-    return sum(individual),
+# IND_SIZE = genes[np.where(genes[:, 2] == 0)].size  # number of non-fixed genes
+IND_SIZE = genes.shape[0]
+POP_SIZE = 100
+CXPB, MUTPB = .5, .2  # crossover probability, mutation probability
 
 
-# ----------
-# Operator registration
-# ----------
-# register the goal / fitness function
-toolbox.register("evaluate", evalOneMax)
+class TestGA:
+    def __init__(self):
+        """
+            Parameters:
+                * PP [0.85, 0.95]
+                * AO [-0.1, 0.1]
+            """
 
-# register the crossover operator
-toolbox.register("mate", tools.cxTwoPoint)
+        # Creator
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        creator.create("Individual", list, fitness=creator.FitnessMin)
 
-# register a mutation operator with a probability to
-# flip each attribute/gene of 0.05
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+        # Toolbox
+        self.toolbox = base.Toolbox()
 
-# operator for selecting individuals for breeding the next
-# generation: each individual of the current generation
-# is replaced by the 'fittest' (best) of three individuals
-# drawn randomly from the current generation.
-toolbox.register("select", tools.selTournament, tournsize=3)
+        # Attribute generator
+        list(self.toolbox.register("attr_%s" % i[3], random.uniform, float(i[0]), float(i[1])) for i in genes)
 
+        # self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_item, n=IND_SIZE)
+        self.toolbox.register("individual", tools.initCycle, creator.Individual,
+                              (self.toolbox.attr_pp, self.toolbox.attr_ao, self.toolbox.attr_div,
+                               self.toolbox.attr_alph1, self.toolbox.attr_alph2, self.toolbox.attr_lambd,
+                               self.toolbox.attr_th, self.toolbox.attr_xmaxth, self.toolbox.attr_xmaxch,
+                               self.toolbox.attr_leth, self.toolbox.attr_teth), n=1)
 
-# ----------
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
 
-def main():
-    random.seed(64)
+        self.toolbox.register("evaluate", self.eval)
+        # self.toolbox.register("evaluate", benchmarks.ackley)
+        self.toolbox.register("mate", tools.cxTwoPoint)
+        self.toolbox.register("mutate", self.mutRestricted, indpb=.3)
+        # self.toolbox.register("mutate", tools.mutFlipBit, indpb=.05)
+        self.toolbox.register("select", tools.selTournament, tournsize=3)
 
-    # create an initial population of 300 individuals (where
-    # each individual is a list of integers)
-    pop = toolbox.population(n=300)
+        self.populate()
 
-    # CXPB  is the probability with which two individuals
-    #       are crossed
-    #
-    # MUTPB is the probability for mutating an individual
-    CXPB, MUTPB = 0.5, 0.2
+    def eval(self, individual):
+        # individuals will be fed to Fine, return omega to minimize
 
-    print("Start of evolution")
+        return sum(individual) / sum([float(i[0]) for i in genes]),
 
-    # Evaluate the entire population
-    fitnesses = list(map(toolbox.evaluate, pop))
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
+    def mutRestricted(self, individual, indpb):
+        if random.random() < indpb:
+            for i, gene in enumerate(genes):
+                individual[i] = random.uniform(float(gene[0]), float(gene[1]))
+        return individual,
+    
 
-    print("  Evaluated %i individuals" % len(pop))
-
-    # Extracting all the fitnesses of
-    fits = [ind.fitness.values[0] for ind in pop]
-
-    # Variable keeping track of the number of generations
-    g = 0
-
-    # Begin the evolution
-    while max(fits) < 100 and g < 1000:
-        # A new generation
-        g = g + 1
-        print("-- Generation %i --" % g)
-
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        offspring = list(map(toolbox.clone, offspring))
-
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-
-            # cross two individuals with probability CXPB
-            if random.random() < CXPB:
-                toolbox.mate(child1, child2)
-
-                # fitness values of the children
-                # must be recalculated later
-                del child1.fitness.values
-                del child2.fitness.values
-
-        for mutant in offspring:
-
-            # mutate an individual with probability MUTPB
-            if random.random() < MUTPB:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
-
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
+    def populate(self):
+        pop = self.toolbox.population(n=POP_SIZE)
+        # evaluate population
+        fitnesses = list(map(self.toolbox.evaluate, pop))
+        for ind, fit in zip(pop, fitnesses):
             ind.fitness.values = fit
 
-        print("  Evaluated %i individuals" % len(invalid_ind))
-
-        # The population is entirely replaced by the offspring
-        pop[:] = offspring
-
-        # Gather all the fitnesses in one list and print the stats
+        # extract fitnesses
         fits = [ind.fitness.values[0] for ind in pop]
 
-        length = len(pop)
-        mean = sum(fits) / length
-        sum2 = sum(x * x for x in fits)
-        std = abs(sum2 / length - mean ** 2) ** 0.5
+        # number of generations
+        g = 0
+        minlist = []
 
-        print("  Min %s" % min(fits))
-        print("  Max %s" % max(fits))
-        print("  Avg %s" % mean)
-        print("  Std %s" % std)
+        while min(fits) > 0 and g < 1000:
+            # new generation
+            g += 1
+            print("-- Generation %i --" % g)
 
-    print("-- End of (successful) evolution --")
+            # select next generation individuals
+            offspring = self.toolbox.select(pop, len(pop))
+            # clone selected individual
+            offspring = list(map(self.toolbox.clone, offspring))
 
-    best_ind = tools.selBest(pop, 1)[0]
-    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+            # crossover and mutations
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < CXPB:
+                    self.toolbox.mate(child1, child2)
+                    del child1.fitness.values
+                    del child2.fitness.values
+
+            for mutant in offspring:
+                if random.random() < MUTPB:
+                    self.toolbox.mutate(mutant)
+                    del mutant.fitness.values
+
+            # evaluate individuals with invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = map(self.toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+            # replace entire existing population with offspring
+            pop[:] = offspring
+
+            # Gather all the fitnesses in one list and print the stats
+            fits = [ind.fitness.values[0] for ind in pop]
+
+            length = len(pop)
+            mean = sum(fits) / length
+            sum2 = sum(x * x for x in fits)
+            std = abs(sum2 / length - mean ** 2) ** 0.5
+
+            print("  Min %s" % min(fits))
+            print("  Max %s" % max(fits))
+            print("  Avg %s" % mean)
+            print("  Std %s" % std)
+            minlist.append(min(fits))
+
+        print("-- End of (successful) evolution --")
+
+        best_ind = tools.selBest(pop, 1)[0]
+        print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
+        plt.plot(minlist)
+        plt.show()
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    # random.seed(123)
+    TestGA()
