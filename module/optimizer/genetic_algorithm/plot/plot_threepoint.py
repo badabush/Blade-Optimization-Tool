fixing rotation of y label in contour plots

module/optimizer/genetic_algorithm/plot/plot_threepoint.py
@@ -15,12 +15,16 @@ def three_point(ds, ref_blade, logdir):
     ref_model_fn = np.poly1d(ref_model)
     ref_dist = max([abs(ref_beta[0]-ref_beta[1]), abs(ref_beta[0]-ref_beta[1])])
     ref_x_s = np.linspace(ref_beta[0]-ref_dist, ref_beta[0]+ref_dist, 30)
-    ax.plot(ref_x_s, ref_model_fn(ref_x_s))
+    ax.plot(ref_x_s, ref_model_fn(ref_x_s), label="ref blade")
     
     best_model = np.polyfit(best_beta, best_omega, 2)
     best_model_fn = np.poly1d(best_model)
     best_dist = max([abs(best_beta[0]-best_beta[1]), abs(best_beta[0]-best_beta[1])])
     best_x_s = np.linspace(best_beta[0]-best_dist, best_beta[0]+best_dist, 30)
-    ax.plot(best_x_s, best_model_fn(best_x_s))
+    ax.plot(best_x_s, best_model_fn(best_x_s), label="best blade")
+    ax.legend()
+    ax.set_xlabel("Beta")
+    ax.set_ylabel("Omega")
+
     
     fig.savefig(Path(logdir + "/ref_best_three_point.png"))