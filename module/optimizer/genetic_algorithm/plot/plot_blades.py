ec4fb06d3e14b4049635d57f89ea7c13ab850524
Ga Man Liang <g.liang@campus.tu-berlin.de>
2021-03-26 18:24:37 +0100

    ref #40 : editing docstring of plot_blades

--- module/optimizer/genetic_algorithm/plot/plot_blades.py
+++ module/optimizer/genetic_algorithm/plot/plot_blades.py
@@ -7,6 +7,17 @@ from UI.blade.blade_plots import bladePlot
 from blade.bladetools import get_blade_from_csv
 
 def deap_blade(blades, logdir):
+    """
+    DEAP Visualization Plot for blade shapes (reference blade/best blade from GA). Saves figure as blades.png in log
+    directory logdir.
+
+    :param blades: list of blades
+    :type blades: list
+    :param logdir: path of processed log where figure should be saved to
+    :type logdir: string
+    
+    :return:
+    """
     default_blade_path = Path(os.getcwd() + "/UI/config/default_blade.csv")
     default_blade = get_blade_from_csv(default_blade_path)
     # plot blades
